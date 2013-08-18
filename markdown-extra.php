76238788768a6c962508154caea02a371e3a1086
Liu Yue <github@euyuil.com>
2013-08-18 19:26:32 +0800

    Removed \n after code blocks so that no extra empty line will be printed.

--- markdown-extra.php
+++ markdown-extra.php
@@ -1030,7 +1030,7 @@ class Markdown_Parser {
 		# trim leading newlines and trailing newlines
 		$codeblock = preg_replace('/\A\n+|\n+\z/', '', $codeblock);
 
-		$codeblock = "<pre><code>$codeblock\n</code></pre>";
+		$codeblock = "<pre><code>$codeblock</code></pre>";
 		return "\n\n".$this->hashBlock($codeblock)."\n\n";
 	}
 
